ec37220e207d3105817828e23ebb97234c0366d6
hetarex0834 <hetarex0834@gmail.com>
2023-01-18 23:39:45 +0900

    コンボボックスを設定

--- SimpleHKBook/MainWindow.xaml.cs
+++ SimpleHKBook/MainWindow.xaml.cs
@@ -23,6 +23,46 @@ namespace SimpleHKBook
         public MainWindow()
         {
             InitializeComponent();
+
+            SetCmbCategoryItem();
+        }
+
+        private void SetCmbCategoryItem()
+        {
+            var items = new Dictionary<string, string[]>
+            {
+                // 支出
+                {
+                    "expense",
+                    new string[]
+                    {
+                        "食費",
+                        "日用雑貨",
+                        "家賃・住宅ローン",
+                        "水道光熱費",
+                        "保険料",
+                        "医療費",
+                        "通信費",
+                        "交通費",
+                        "教育費",
+                        "衣服費",
+                        "交際費",
+                        "趣味",
+                        "その他",
+                    }
+                },
+                // 収入
+                {
+                    "income",
+                    new string[]
+                    {
+                        "給料",
+                        "その他",
+                    }
+                },
+            };
+
+            foreach (var item in items["expense"]) cmbCategory.Items.Add(item);
         }
     }
 }
