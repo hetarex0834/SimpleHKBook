8f397d98bf9ced4e636c1bff2acbddc57c6b1875
hetarex0834 <hetarex0834@gmail.com>
2023-01-19 23:29:46 +0900

    ファイル保存機能を作成

--- SimpleHKBook/MainWindow.xaml.cs
+++ SimpleHKBook/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace SimpleHKBook
 {
@@ -21,7 +25,9 @@ namespace SimpleHKBook
     /// </summary>
     public partial class MainWindow : Window
     {
-        // カテゴリ
+        // データテーブル
+        private readonly DataTable dt = new("HKBookData");
+        // 収支区分
         private readonly string expense = "expense"; // 支出
         private readonly string income = "income"; // 収入
 
@@ -31,15 +37,24 @@ namespace SimpleHKBook
         public MainWindow()
         {
             InitializeComponent();
-
             SetCmbCategoryItem(expense);
+
+            var clms = new Dictionary<string, Type>
+            {
+                { "date", typeof(DateTime) },
+                { "group", typeof(string) },
+                { "category", typeof(object) },
+                { "amount", typeof(int) },
+                { "memo", typeof(string) },
+            };
+            foreach (var c in clms) dt.Columns.Add(c.Key, c.Value);
         }
 
         /// <summary>
         /// コンボボックスにアイテムを設定
         /// </summary>
-        /// <param name="category">カテゴリ</param>
-        private void SetCmbCategoryItem(in string category)
+        /// <param name="group">区分</param>
+        private void SetCmbCategoryItem(in string group)
         {
             var items = new Dictionary<string, string[]>
             {
@@ -49,17 +64,21 @@ namespace SimpleHKBook
                     new string[]
                     {
                         "食費",
-                        "日用雑貨",
-                        "家賃・住宅ローン",
+                        "日用品費",
+                        "住居費",
                         "水道光熱費",
-                        "保険料",
-                        "医療費",
                         "通信費",
+                        "保険料",
+                        "車両費",
+                        "学費",
+                        "税金",
                         "交通費",
-                        "教育費",
-                        "衣服費",
+                        "医療費",
+                        "被服費",
+                        "美容費",
                         "交際費",
-                        "趣味",
+                        "娯楽費",
+                        "小遣い",
                         "その他",
                     }
                 },
@@ -75,12 +94,12 @@ namespace SimpleHKBook
             };
 
             cmbCategory.Items.Clear();
-            foreach (var item in items[category]) cmbCategory.Items.Add(item);
+            foreach (var item in items[group]) cmbCategory.Items.Add(item);
             cmbCategory.SelectedIndex = 0;
         }
 
         /// <summary>
-        /// ラジオボタンの値に応じてコンボボックスのカテゴリを変更
+        /// ラジオボタンの値に応じてコンボボックスの区分を変更
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -95,20 +114,50 @@ namespace SimpleHKBook
         }
 
         /// <summary>
-        /// 数字以外のテキストボックスへの入力を制限
+        /// 数字以外のtxtAmountへの入力を制限
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void TxtAmount_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = !new Regex("[0-9]").IsMatch(e.Text);
 
         /// <summary>
-        /// テキストボックスへの貼り付けを禁止
+        /// txtAmountへの貼り付けを禁止
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void TxtAmount_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             if (e.Command == ApplicationCommands.Paste) e.Handled = true;
+        }
+
+        /// <summary>
+        /// 入力結果を保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtAmount.Text == "")
+            {
+                MessageBox.Show("金額を入力してください。", "金額未入力", MessageBoxButton.OK, MessageBoxImage.Stop);
+                return;
+            }
+
+            var row = dt.NewRow();
+            row["date"] = dp.SelectedDate;
+            row["group"] = (rbtExpense.IsChecked == true) ? "支出" : "収入";
+            row["category"] = cmbCategory.SelectedItem;
+            row["amount"] = int.Parse(txtAmount.Text);
+            row["memo"] = txtMemo.Text;
+            dt.Rows.Add(row);
+
+            using var fs = new FileStream("HKBookData.xml", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            var serializer = new XmlSerializer(dt.GetType());
+            serializer.Serialize(fs, dt);
+
+            MessageBox.Show("保存しました。", "保存完了", MessageBoxButton.OK, MessageBoxImage.Information);
+            txtAmount.Text = "";
+            txtMemo.Text = "";
         }
     }
 }
