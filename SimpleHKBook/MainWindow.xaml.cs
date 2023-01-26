98a0f950e7808ec263c98b2b36556eb2e8e791f2
hetarex0834 <hetarex0834@gmail.com>
2023-01-26 23:36:52 +0900

    コーディング完了

--- SimpleHKBook/MainWindow.xaml.cs
+++ SimpleHKBook/MainWindow.xaml.cs
@@ -51,12 +51,12 @@ namespace SimpleHKBook
             }
             finally
             {
-                ExtractRecordsByDate();
+                ExtractRecord();
             }
         }
 
         /// <summary>
-        /// コンボボックスにアイテムを設定
+        /// ComboBoxにアイテムを設定
         /// </summary>
         /// <param name="group">区分</param>
         private void SetCmbCategoryItem(in string group)
@@ -124,7 +124,7 @@ namespace SimpleHKBook
         }
 
         /// <summary>
-        /// DataTableの内容をXML形式でファイル保存
+        /// DataTableの内容をXML形式で保存
         /// </summary>
         private void SaveDataTableToXml()
         {
@@ -144,16 +144,40 @@ namespace SimpleHKBook
         }
 
         /// <summary>
-        /// テキストボックスに集計結果を設定
+        /// 1ヶ月分のレコードを抽出
+        /// </summary>
+        private void ExtractRecord()
+        {
+            // 年月をLabelに設定
+            lblSelectedDate.DataContext = (month < 10) ? $"{year}年0{month}月" : $"{year}年{month}月";
+
+            // 1ヶ月分のレコードをDataGridに設定
+            var dt = this.dt.Clone();
+            var rows = this.dt.AsEnumerable()
+                            .Where(x => int.Parse($"{x["日付"]}".Split('/')[0]) == year)
+                            .Where(x => int.Parse($"{x["日付"]}".Split('/')[1]) == month);
+            foreach (var row in rows) dt.ImportRow(row);
+            dgd.DataContext = dt;
+
+            // 集計結果を設定
+            SetTotalValue();
+        }
+
+        /// <summary>
+        /// TextBoxに集計結果を設定
         /// </summary>
         private void SetTotalValue()
         {
-            int cf = 0, total = 0;
+            // 繰越金
+            var cf = 0;
             var dtCf = dt.Clone();
-            var dtTotal = dt.Clone();
             var cfRows = dt.AsEnumerable();
+            // 合計
+            var total = 0;
+            var dtTotal = dt.Clone();
             var totalRows = dt.AsEnumerable();
 
+            // 前期分のレコードを抽出
             if (month == 1)
             {
                 cfRows = cfRows.Where(x => int.Parse($"{x["日付"]}".Split('/')[0]) < year);
@@ -162,11 +186,13 @@ namespace SimpleHKBook
             {
                 cfRows = cfRows.Where(x => int.Parse($"{x["日付"]}".Split('/')[0] + $"{x["日付"]}".Split('/')[1]) < year * 100 + month);
             }
+            foreach (var r in cfRows) dtCf.ImportRow(r);
+            // 当月分のレコードを抽出
             totalRows = totalRows.Where(x => int.Parse($"{x["日付"]}".Split('/')[0]) == year)
                               .Where(x => int.Parse($"{x["日付"]}".Split('/')[1]) == month);
-            foreach (var r in cfRows) dtCf.ImportRow(r);
             foreach (var r in totalRows) dtTotal.ImportRow(r);
 
+            // 繰越金を集計
             foreach (DataRow r in dtCf.Rows)
             {
                 switch (r["区分"])
@@ -179,6 +205,7 @@ namespace SimpleHKBook
                         break;
                 }
             }
+            // 当月分の合計を集計
             foreach (DataRow r in dtTotal.Rows)
             {
                 switch (r["区分"])
@@ -192,31 +219,15 @@ namespace SimpleHKBook
                 }
             }
 
-            txtCf.Text = $"{cf:#,0}";
-            txtTotal.Text = $"{cf + total:#,0}";
+            // 集計結果を設定
+            txtCf.Text = $"{cf:#,0}"; // 繰越金
+            txtTotal.Text = $"{cf + total:#,0}"; // 合計
         }
 
         /// <summary>
-        /// 1ヶ月分のレコードを抽出
+        /// RadioButtonの値に応じてComboBoxの区分を変更
         /// </summary>
-        private void ExtractRecordsByDate()
-        {
-            lblSelectedDate.DataContext = (month < 10) ? $"{year}年0{month}月" : $"{year}年{month}月";
-
-            var dt = this.dt.Clone();
-            var rows = this.dt.AsEnumerable()
-                            .Where(x => int.Parse($"{x["日付"]}".Split('/')[0]) == year)
-                            .Where(x => int.Parse($"{x["日付"]}".Split('/')[1]) == month);
-
-            foreach (var row in rows) dt.ImportRow(row);
-            dgd.DataContext = dt;
-            SetTotalValue();
-        }
-
-        /// <summary>
-        /// ラジオボタンの値に応じてコンボボックスの区分を変更
-        /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">RadioButton</param>
         /// <param name="e"></param>
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -236,14 +247,14 @@ namespace SimpleHKBook
         private void TxtAmount_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = new Regex("[^0-9]").IsMatch(e.Text);
 
         /// <summary>
-        /// txtAmountへの貼り付けを禁止
+        /// txtAmountへの貼り付けを制限
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void TxtAmount_PreviewExecuted(object sender, ExecutedRoutedEventArgs e) => e.Handled = e.Command == ApplicationCommands.Paste;
 
         /// <summary>
-        /// 入力結果を保存
+        /// 入力内容を保存
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -257,15 +268,16 @@ namespace SimpleHKBook
 
             try
             {
-                int id = (dt.Rows.Count > 0) ? Convert.ToInt32(dt.Compute("MAX(ID)", null)) + 1 : 1;
+                // 入力内容をDataTableに追加
                 var row = dt.NewRow();
-                row["ID"] = id;
+                row["ID"] = (dt.Rows.Count > 0) ? Convert.ToInt32(dt.Compute("MAX(ID)", null)) + 1 : 1;
                 row["日付"] = $"{dp.SelectedDate:yyyy/MM/dd}";
                 row["区分"] = (rbtExpense.IsChecked == true) ? "支出" : "収入";
                 row["カテゴリ"] = $"{cmbCategory.SelectedItem}";
                 row["金額"] = int.Parse(txtAmount.Text);
                 row["メモ"] = txtMemo.Text;
                 dt.Rows.Add(row);
+                // ファイル保存
                 SaveDataTableToXml();
 
                 MessageBox.Show("保存しました。", "保存完了", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -274,6 +286,7 @@ namespace SimpleHKBook
             }
             catch
             {
+                // XMLファイルを初期化
                 if (MessageBox.Show("保存ファイルとテーブルの整合性が確認できませんでした。\nファイルを初期化しますか？",
                                                "保存ファイル不整合", MessageBoxButton.OKCancel, MessageBoxImage.Error) == MessageBoxResult.OK)
                 {
@@ -284,27 +297,37 @@ namespace SimpleHKBook
             }
             finally
             {
-                ExtractRecordsByDate();
+                ExtractRecord();
             }
         }
 
         /// <summary>
-        /// 修正・削除用ウインドウを表示
+        /// 修正・削除用モーダルウインドウを表示
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Dgd_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            if (dgd.SelectedItem != null)
+            try
             {
-                var window = new ModalWindow(dgd)
+                if (dgd.SelectedItem != null)
                 {
-                    Owner = GetWindow(this),
-                };
-                window.ShowDialog();
+                    var window = new ModalWindow(dgd)
+                    {
+                        Owner = GetWindow(this),
+                    };
+                    window.ShowDialog();
+                }
+                LoadXmlToDataTable();
             }
-            LoadXmlToDataTable();
-            ExtractRecordsByDate();
+            catch
+            {
+                SaveDataTableToXml();
+            }
+            finally
+            {
+                ExtractRecord();
+            }
         }
 
         /// <summary>
@@ -320,7 +343,7 @@ namespace SimpleHKBook
                 year--;
             }
             else month--;
-            ExtractRecordsByDate();
+            ExtractRecord();
         }
 
         /// <summary>
@@ -336,7 +359,7 @@ namespace SimpleHKBook
                 year++;
             }
             else month++;
-            ExtractRecordsByDate();
+            ExtractRecord();
         }
     }
 }
--- SimpleHKBook/ModalWindow.xaml.cs
+++ SimpleHKBook/ModalWindow.xaml.cs
@@ -19,7 +19,7 @@ using System.Xml.Linq;
 namespace SimpleHKBook
 {
     /// <summary>
-    /// 修正・削除用ウインドウ
+    /// 修正・削除用モーダルウインドウ
     /// ModalWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class ModalWindow : Window
@@ -28,13 +28,19 @@ namespace SimpleHKBook
         private readonly string expense = "expense"; // 支出
         private readonly string income = "income"; // 収入
 
+        /// <summary>
+        /// コントロールにDataGridの内容を設定
+        /// </summary>
+        /// <param name="dgd">DataGrid</param>
         public ModalWindow(DataGrid dgd)
         {
             InitializeComponent();
 
+            // レコードID
             id = ((TextBlock)dgd.Columns[0].GetCellContent(dgd.SelectedItem)).Text;
+            // DatePicker
             dp.Text = ((TextBlock)dgd.Columns[1].GetCellContent(dgd.SelectedItem)).Text;
-
+            // RadioButton
             switch (((TextBlock)dgd.Columns[2].GetCellContent(dgd.SelectedItem)).Text)
             {
                 case "支出":
@@ -44,7 +50,7 @@ namespace SimpleHKBook
                     rbtIncome.IsChecked = true;
                     break;
             }
-
+            // ComboBox
             for (var i = 0; i < cmbCategory.Items.Count; i++)
             {
                 if ($"{cmbCategory.Items[i]}" == ((TextBlock)dgd.Columns[3].GetCellContent(dgd.SelectedItem)).Text)
@@ -53,13 +59,13 @@ namespace SimpleHKBook
                     break;
                 }
             }
-
+            // TextBox
             txtAmount.Text = ((TextBlock)dgd.Columns[4].GetCellContent(dgd.SelectedItem)).Text;
             txtMemo.Text = ((TextBlock)dgd.Columns[5].GetCellContent(dgd.SelectedItem)).Text;
         }
         
         /// <summary>
-        /// コンボボックスにアイテムを設定
+        /// ComboBoxにアイテムを設定
         /// </summary>
         /// <param name="group">区分</param>
         private void SetCmbCategoryItem(in string group)
@@ -107,9 +113,9 @@ namespace SimpleHKBook
         }
 
         /// <summary>
-        /// ラジオボタンの値に応じてコンボボックスの区分を変更
+        /// RadioButtonの値に応じてComboBoxの区分を変更
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">RadioButton</param>
         /// <param name="e"></param>
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
@@ -129,14 +135,14 @@ namespace SimpleHKBook
         private void TxtAmount_PreviewTextInput(object sender, TextCompositionEventArgs e) => e.Handled = new Regex("[^0-9]").IsMatch(e.Text);
 
         /// <summary>
-        /// txtAmountへの貼り付けを禁止
+        /// txtAmountへの貼り付けを制限
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void TxtAmount_PreviewExecuted(object sender, ExecutedRoutedEventArgs e) => e.Handled = e.Command == ApplicationCommands.Paste;
 
         /// <summary>
-        /// 修正結果を保存
+        /// 修正内容を保存
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -148,16 +154,18 @@ namespace SimpleHKBook
                 return;
             }
 
+            // XMLファイル
             var xml = XElement.Load("HKBookData.xml");
-            var info = xml.Descendants("HKBookData")
-                           .Where(x => x.Element("ID")!.Value == id)
-                           .Select(x => x).Single();
-
-            info.Element("日付")!.Value = $"{dp.SelectedDate:yyyy/MM/dd}";
-            info.Element("区分")!.Value = (rbtExpense.IsChecked == true) ? "支出" : "収入";
-            info.Element("カテゴリ")!.Value = $"{cmbCategory.SelectedItem}";
-            info.Element("金額")!.Value = $"{int.Parse(txtAmount.Text)}";
-            info.Element("メモ")!.Value = txtMemo.Text;
+            // 修正対象
+            var elem = xml.Descendants("HKBookData")
+                             .Where(x => x.Element("ID")!.Value == id)
+                             .Select(x => x).Single();
+            // 修正内容をXMLファイルへ保存
+            elem.Element("日付")!.Value = $"{dp.SelectedDate:yyyy/MM/dd}";
+            elem.Element("区分")!.Value = (rbtExpense.IsChecked == true) ? "支出" : "収入";
+            elem.Element("カテゴリ")!.Value = $"{cmbCategory.SelectedItem}";
+            elem.Element("金額")!.Value = $"{int.Parse(txtAmount.Text)}";
+            elem.Element("メモ")!.Value = txtMemo.Text;
             xml.Save("HKBookData.xml");
 
             MessageBox.Show("保存しました。", "保存完了", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -171,13 +179,16 @@ namespace SimpleHKBook
         /// <param name="e"></param>
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
+            // XMLファイル
             var xml = XElement.Load("HKBookData.xml");
-            var info = xml.Descendants("HKBookData")
-                           .Where(x => x.Element("ID")!.Value == id)
-                           .Select(x => x).Single();
-
-            info.Remove();
+            // 削除対象
+            var elem = xml.Descendants("HKBookData")
+                             .Where(x => x.Element("ID")!.Value == id)
+                             .Select(x => x).Single();
+            // 対象をXMLファイルから削除
+            elem.Remove();
             xml.Save("HKBookData.xml");
+
             MessageBox.Show("削除しました。", "削除完了", MessageBoxButton.OK, MessageBoxImage.Information);
             Close();
         }
